d1b0f591b3a6010ddad1db6930497c59a3c3008a
Albert Shih <albertshih3@gmail.com>
2022-12-26 16:17:34 -0800

    added badge from lesson 4

---
+++ Landmarks Tutorial App/Views/Badges/RotatedBadgeSymbol.swift
@@ -0,0 +1,24 @@
+//
+//  RotatedBadgeSymbol.swift
+//  Landmarks Tutorial App
+//
+//  Created by Kaelin Shih on 12/26/22.
+//
+
+import SwiftUI
+
+struct RotatedBadgeSymbol: View {
+    let angle: Angle
+    
+    var body: some View {
+        BadgeSymbol()
+            .padding(-60)
+            .rotationEffect(angle, anchor: .bottom)
+    }
+}
+
+struct RotatedBadgeSymbol_Previews: PreviewProvider {
+    static var previews: some View {
+        RotatedBadgeSymbol(angle: Angle(degrees: 5))
+    }
+}
